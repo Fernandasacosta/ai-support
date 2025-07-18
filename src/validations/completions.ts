e6403ac3cf783f159bb74f2d51552b8633a07398
Fernandasacosta <fernandasacostaa@gmail.com>
2025-07-18 16:10:23 -0300

    feat: add completions validaton

---
+++ src/validations/completions.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const messageSchema = z.object({
+  role: z.enum(['USER', 'AGENT']),
+  content: z.string(),
+});
+
+const completionsSchema = z.object({
+  helpdeskId: z.number(),
+  projectName: z.enum(['tesla_motors']),
+  messages: z.array(messageSchema),
+});
+
+type Message = z.infer<typeof messageSchema>
+
+export { messageSchema, completionsSchema, Message }
